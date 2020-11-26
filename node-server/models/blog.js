7a39b5f1a546d0cd74aaf1bc5f7ab39d3db50706
Partha <parthapaul47@gmail.com>
2020-11-26 16:51:32 +0530

    Reviewed Blog Part, Made some changes, Requesting testing by Vaibhav

--- node-server/models/blog.js
+++ node-server/models/blog.js
@@ -2,25 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        // required: true
-    },
-    userName: {
-        type: String,
-        // required: true
-    },
-    title: {
-        type: String,
-        // required: true
-    },
-    description: {
-        type: String,
-        // required: true
-    },
-    imageUrl: {
-        type: String,
-    }
+  userId: {
+    type: Schema.Types.ObjectId,
+    // required: true
+  },
+  userName: {
+    type: String,
+    // required: true
+  },
+  title: {
+    type: String,
+    // required: true
+  },
+  description: {
+    type: String,
+    // required: true
+  },
+  imageUrl: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("blog", blogSchema);
