e7417be5026af3a0149434e4dfa15b29778f2b77
nipusingla <piyushsingla2416@gmail.com>
2020-11-27 02:55:36 +0530

    added code

--- HomeTreatment/lib/main.dart
+++ HomeTreatment/lib/main.dart
@@ -7,7 +7,34 @@ import 'package:hexcolor/hexcolor.dart';
 import './screens/HomeScreen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin=FlutterLocalNotificationsPlugin();
+
+void main()async  {
+
+WidgetsFlutterBinding.ensureInitialized();
+
+ WidgetsFlutterBinding.ensureInitialized();
+
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('app_logo');
+  var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification:
+          (int id, String title, String body, String payload) async {});
+  var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+  });
+
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider<Auth>(create: (_) => Auth())],
--- HomeTreatment/lib/screens/AppointmentScreen.dart
+++ HomeTreatment/lib/screens/AppointmentScreen.dart
@@ -3,11 +3,15 @@ import 'package:HomeTreatment/model/hospitalModel.dart';
 import 'package:HomeTreatment/provider/auth.dart';
 import 'package:HomeTreatment/screens/MainScreen.dart';
 import 'package:HomeTreatment/widgets/AppBarWidget.dart';
+import 'package:HomeTreatment/widgets/ProgessBar.dart';
 import 'package:clipboard/clipboard.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_search_bar/flutter_search_bar.dart';
 import 'package:provider/provider.dart';
+
+import '../main.dart';
 
 class AppointmentScreen extends StatefulWidget {
   static const routeName = '/appointment-screen';
@@ -43,152 +47,208 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
     isInit = true;
   }
 
+  void loading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  bool _isLoading = false;
+
+  void scheduleAlarm(
+      DateTime scheduledNotificationDateTime, String appointment) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'alarm_notif',
+      'alarm_notif',
+      'Channel for Alarm notification',
+      icon: 'app_logo',
+      sound: RawResourceAndroidNotificationSound('a_long_cold_sting'),
+      largeIcon: DrawableResourceAndroidBitmap('app_logo'),
+    );
+
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails(
+        sound: 'a_long_cold_sting.wav',
+        presentAlert: true,
+        presentBadge: true,
+        presentSound: true);
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.schedule(0, 'Appointment',
+        appointment, scheduledNotificationDateTime, platformChannelSpecifics);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget.myAppBar(),
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Container(
-        child: ListView.builder(
-          itemCount: m.possibleTimes.length,
-          itemBuilder: (context, index) {
-            if (m.possibleTimes[index].status == true) {
-              return SizedBox(
-                width: 0,
-              );
-            } else {
-              print("i am in else");
-              return InkWell(
-                onTap: () {
-                  print("on tap");
-                },
-                child: RaisedButton(
-                  onPressed: () {
-                    print("hello");
-                    showDialog(
-                      context: context,
-                      builder: (_) => Container(
-                        width: 200,
-                        child: new AlertDialog(
-                          title: new Text(
-                            "Choose Type",
-                            style: TextStyle(
-                                color: Theme.of(context).backgroundColor),
-                          ),
-                          actions: <Widget>[
-                            RaisedButton.icon(
-                              textColor: Colors.white,
-                              color: Theme.of(context).primaryColor,
-                              onPressed: () async {
-                                bool flag = await Provider.of<Auth>(context,
-                                        listen: false)
-                                    .makeAppointment(
-                                        m.id, m.possibleTimes[index].timeSlot);
-                                if (flag) {
-                                  Navigator.of(context).pop();
-                                  Navigator.of(context).pushReplacementNamed(
-                                      MainScreen.routeName);
-                                } else {
-                                  Scaffold.of(context).showSnackBar(SnackBar(
-                                      content: Text('Error Try Again')));
-                                }
-                              },
-                              icon: Icon(Icons.add, size: 14),
-                              label: Text("Offline"),
-                            ),
-                            RaisedButton.icon(
-                              textColor: Colors.white,
-                              color: Theme.of(context).primaryColor,
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (_) => Container(
-                                    child: AlertDialog(
-                                      title: Text(
-                                        m.meetLink,
-                                        style: TextStyle(
-                                            color: Theme.of(context)
-                                                .backgroundColor),
-                                      ),
-                                      actions: [
-                                        RaisedButton.icon(
-                                          textColor: Colors.white,
-                                          color: Theme.of(context).primaryColor,
-                                          onPressed: () async {
-                                            bool flag = await Provider.of<Auth>(
-                                                    context,
-                                                    listen: false)
-                                                .makeAppointment(
-                                                    m.id,
-                                                    m.possibleTimes[index]
-                                                        .timeSlot);
-                                            if (flag) {
-                                              FlutterClipboard.copy(m.meetLink)
-                                                  .then((value) {
-                                                Navigator.of(context).pop();
-                                                Scaffold.of(context)
-                                                    .showSnackBar(SnackBar(
-                                                        content:
-                                                            Text('Copied')));
-                                                Navigator.of(context).pop();
-                                                Navigator.of(context).pop();
-                                                Navigator.of(context)
-                                                    .pushReplacementNamed(
-                                                        MainScreen.routeName);
-                                              });
-                                            } else {
-                                              print("error in booking");
-                                            }
-                                          },
-                                          icon: Icon(Icons.add, size: 18),
-                                          label: Text("Copy"),
-                                        ),
-                                        RaisedButton.icon(
-                                          textColor: Colors.white,
-                                          color: Theme.of(context).primaryColor,
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          icon: Icon(Icons.add, size: 18),
-                                          label: Text("Close"),
-                                        ),
-                                      ],
-                                    ),
+      body: _isLoading
+          ? ProgessBar()
+          : Container(
+              child: ListView.builder(
+                itemCount: m.possibleTimes.length,
+                itemBuilder: (context, index) {
+                  if (m.possibleTimes[index].status == true) {
+                    return SizedBox(
+                      width: 0,
+                    );
+                  } else {
+                    print("i am in else");
+                    return InkWell(
+                      onTap: () {
+                        print("on tap");
+                      },
+                      child: RaisedButton(
+                        onPressed: () {
+                          print("hello");
+                          showDialog(
+                            context: context,
+                            builder: (_) => Container(
+                              width: 200,
+                              child: new AlertDialog(
+                                title: new Text(
+                                  "Choose Type",
+                                  style: TextStyle(
+                                      color: Theme.of(context).backgroundColor),
+                                ),
+                                actions: <Widget>[
+                                  RaisedButton.icon(
+                                    textColor: Colors.white,
+                                    color: Theme.of(context).primaryColor,
+                                    onPressed: () async {
+                                      bool flag = await Provider.of<Auth>(
+                                              context,
+                                              listen: false)
+                                          .makeAppointment(m.id,
+                                              m.possibleTimes[index].timeSlot);
+
+                                      if (flag) {
+                                        Navigator.of(context).pop();
+                                        Navigator.of(context)
+                                            .pushReplacementNamed(
+                                                MainScreen.routeName);
+                                        scheduleAlarm(
+                                            DateTime.now()
+                                                .add(new Duration(seconds: 20)),
+                                            "Hurry Go");
+                                      } else {
+                                        Scaffold.of(context).showSnackBar(
+                                            SnackBar(
+                                                content:
+                                                    Text('Error Try Again')));
+                                      }
+                                    },
+                                    icon: Icon(Icons.add, size: 14),
+                                    label: Text("Offline"),
                                   ),
-                                );
-                              },
-                              icon: Icon(Icons.add, size: 14),
-                              label: Text("Online"),
+                                  RaisedButton.icon(
+                                    textColor: Colors.white,
+                                    color: Theme.of(context).primaryColor,
+                                    onPressed: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (_) => Container(
+                                          child: AlertDialog(
+                                            title: Text(
+                                              m.meetLink,
+                                              style: TextStyle(
+                                                  color: Theme.of(context)
+                                                      .backgroundColor),
+                                            ),
+                                            actions: [
+                                              RaisedButton.icon(
+                                                textColor: Colors.white,
+                                                color: Theme.of(context)
+                                                    .primaryColor,
+                                                onPressed: () async {
+                                                  bool flag = await Provider.of<
+                                                              Auth>(context,
+                                                          listen: false)
+                                                      .makeAppointment(
+                                                          m.id,
+                                                          m.possibleTimes[index]
+                                                              .timeSlot);
+
+                                                  if (flag) {
+                                                    FlutterClipboard.copy(
+                                                            m.meetLink)
+                                                        .then((value) {
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                      Scaffold.of(context)
+                                                          .showSnackBar(SnackBar(
+                                                              content: Text(
+                                                                  'Copied')));
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                      Navigator.of(context)
+                                                          .pushReplacementNamed(
+                                                              MainScreen
+                                                                  .routeName);
+                                                    });
+
+                                                    scheduleAlarm(
+                                                        DateTime.now().add(
+                                                            new Duration(
+                                                                seconds: 20)),m.meetLink
+                                                        );
+                                                  } else {
+                                                    print("error in booking");
+                                                  }
+                                                },
+                                                icon: Icon(Icons.add, size: 18),
+                                                label: Text("Copy"),
+                                              ),
+                                              RaisedButton.icon(
+                                                textColor: Colors.white,
+                                                color: Theme.of(context)
+                                                    .primaryColor,
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                                icon: Icon(Icons.add, size: 18),
+                                                label: Text("Close"),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    icon: Icon(Icons.add, size: 14),
+                                    label: Text("Online"),
+                                  ),
+                                  RaisedButton.icon(
+                                    textColor: Colors.white,
+                                    color: Theme.of(context).primaryColor,
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    icon: Icon(Icons.add, size: 18),
+                                    label: Text("Close"),
+                                  ),
+                                ],
+                              ),
                             ),
-                            RaisedButton.icon(
-                              textColor: Colors.white,
-                              color: Theme.of(context).primaryColor,
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              icon: Icon(Icons.add, size: 18),
-                              label: Text("Close"),
-                            ),
-                          ],
+                          );
+                        },
+                        color: Colors.grey.shade200,
+                        child: Text(
+                          "${m.possibleTimes[index].timeSlot}-${m.possibleTimes[index].endSlot}",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25),
                         ),
                       ),
                     );
-                  },
-                  color: Colors.grey.shade200,
-                  child: Text(
-                    "${m.possibleTimes[index].timeSlot}-${m.possibleTimes[index].endSlot}",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25),
-                  ),
-                ),
-              );
-            }
-          },
-        ),
-      ),
+                  }
+                },
+              ),
+            ),
     );
   }
 }
