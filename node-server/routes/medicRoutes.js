31f4f5d9fc8ad934ca4da834b27b62333b3fea1e
vxd447 <vdua447@gmail.com>
2020-11-27 02:56:58 +0530

    Adding getIssues route

--- node-server/routes/medicRoutes.js
+++ node-server/routes/medicRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const CryptoJS = require("crypto-js");
 const axios = require("axios");
+const Patient = require("../models/patient");
 
 const {
     Unauthorized,
@@ -13,19 +14,43 @@ const {
 router.get("/getIssues", async (req, res, next) => {
     try {
         const { symptoms } = req.body;
+        const { _id } = req.user;
+        const user = await Patient.findById(_id);
         var computedHash = CryptoJS.HmacMD5(process.env.SYMP_URI, process.env.SYMP_SECRET_KEY);
         var computedHashString = computedHash.toString(CryptoJS.enc.Base64);
         var authValue = "Bearer " + process.env.SYMP_API_KEY + ":" + computedHashString;
-        var response = await axios.post(process.env.SYMP_URI, {
-            headers: {
-                Authorization : authValue.toString()
-            }  
-        });
-        // console.log(response);
-        return Success(res);
+        var config = {
+            method: 'post',
+            url: process.env.SYMP_URI,
+            headers: { 
+              'Authorization': authValue, 
+              'Cookie': 'ASP.NET_SessionId=1g0haj4hy3ki03laownw1gxr'
+            }
+        };
+        var response = await axios(config);
+        const token = response.data.Token;
+        var medicUrl = 'https://healthservice.priaid.ch/diagnosis?';
+        medicUrl += 'token=' + token;
+        medicUrl += '&';
+        medicUrl += 'language=' + "en-gb";
+        medicUrl += '&';
+        medicUrl += 'symptoms=' + symptoms;
+        medicUrl += '&';
+        medicUrl += 'gender=' + "male";
+        medicUrl += '&';
+        medicUrl += 'year_of_birth=' + '1998';  
+        config = {
+            method: 'get',
+            url: medicUrl,
+            headers: { 
+              'Cookie': 'ASP.NET_SessionId=3sssd2gxip11rfjuxph3lniu'
+            }
+        };
+        response = await axios(config);
+        return Success(res, response.data);
 
     } catch (err) {
-        console.log(err.response.data);
+        console.log(err);
         return SomethingWentWrong(res);
     }
 });
