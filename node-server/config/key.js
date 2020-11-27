7e4228a19c7562cf2c31d770f013dd300d084496
nipusingla <piyushsingla2416@gmail.com>
2020-11-27 11:34:57 +0530

    added only client code

--- node-server/config/key.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  URL:"mongodb+srv://nipun:nipun@cluster0-9ie5b.mongodb.net/test?retryWrites=true&w=majority",
-  secret: "JWT_SECRET=addjsonwebtokensecretherelikeQuiscustodietipsoscustodes",
-  db_name: "Appointment-App",
-};
